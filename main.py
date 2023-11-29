c3bbfd758eafa3bc4763db784a85170b70d89373
Bahaa <0xffvirus@gmail.com>
2023-11-29 20:58:12 +0300

    Added Check

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import random
 def main():
     number = RandomNumber()
     userInput = int(input("Enter Number between (1-9): "))
-    return CheckNumbers(number, userInput)
+    return print(CheckNumbers(number, userInput))
 
 def RandomNumber():
     return random.randint(1,9)
 
 def CheckNumbers(a,b):
-    return None    
-
+    if a == b:
+        return "You WON"    
+    else:
+        return "You Lost!!"
 main()
