57e6071b6f1285e53fc1d3b6483632b16e3b9a7a
Bahaa <0xffvirus@gmail.com>
2023-11-29 20:42:13 +0300

    Added main file without login

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+
+def main():
+    number = RandomNumber()
+    userInput = int(input("Enter Number between (1-9): "))
+    return CheckNumbers(number, userInput)
+
+def RandomNumber():
+    return None
+
+def CheckNumbers(a,b):
+    return None    
+
+main()
